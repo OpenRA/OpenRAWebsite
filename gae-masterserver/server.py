b9e2cfc8b739cc19e9ccc498ec432ef1ed1d79a1
alzeih <alzeih@gmail.com>
2010-12-01 23:06:07 +1300

    catch and log exceptions in the server

--- gae-masterserver/server.py
+++ gae-masterserver/server.py
@@ -12,44 +12,61 @@ class Server:
     STALE_DURATION = datetime.timedelta(seconds=300)
     
     def NewGame(self, name, ip, port, players, state, map, mods):
-        game = Game(name=name, ip=ip, port=int(port), players=int(players), 
-                        state=int(state), map=map, mods=mods)
-        game.put()
+        try:
+            game = Game(name=name, ip=ip, port=int(port), players=int(players), 
+                            state=int(state), map=map, mods=mods)
+            game.put()
+        except Exception, exception:
+            logging.exception(exception)
     NewGame = classmethod(NewGame)
     
     def UpdateGame(self, name, ip, port, players, state, map, mods):
-        now = datetime.datetime.now()
-        games = []
-        game = db.GqlQuery("SELECT * FROM Game where ip = :1 AND port = :2 AND lastPingTime >= :3 ORDER BY lastPingTime DESC", 
-                                    ip, int(port), now - Server.STALE_DURATION).get()
-        if game:
-            game.name = name
-            game.ip = ip
-            game.port = int(port)
-            game.players = int(players)
-            game.state = int(state)
-            game.map = map
-            game.mods = mods
-            game.lastPingTime = now
-            game.put()
+        try:
+            games = []
+            game = db.GqlQuery("SELECT * FROM Game where ip = :1 AND port = :2 AND lastPingTime >= :3 ORDER BY lastPingTime DESC", 
+                                        ip, int(port), now - Server.STALE_DURATION).get()
+            if game:
+                game.name = name
+                game.ip = ip
+                game.port = int(port)
+                game.players = int(players)
+                game.state = int(state)
+                game.map = map
+                game.mods = mods
+                game.put()
+        except Exception, exception:
+            logging.exception(exception)
     UpdateGame = classmethod(UpdateGame)
     
+    def EndGame(self, ip, port):
+        try:
+            #end ALL games on this ip / port
+            games = db.GqlQuery("SELECT * FROM Game where ip = :1 AND port = :2", ip, int(port))
+            for game in games:
+                game.delete()
+        except Exception, exception:
+            logging.exception(exception)
+    EndGame = classmethod(EndGame)
+    
     def GetGames(self):
-        now = datetime.datetime.now()
-        games = []
-        currentGames = db.GqlQuery("SELECT * FROM Game where lastPingTime >= :1 ORDER BY lastPingTime DESC", now - Server.STALE_DURATION)
-        for game in currentGames:
-            ttl = Server.STALE_DURATION.seconds - (now - game.lastPingTime).seconds
-            games.append({
-                      'Name': game.name,
-                      'Address': "%s:%d" % (game.ip, game.port),
-                      'State': str(game.state),
-                      'Players': str(game.players),
-                      'Map': game.map,
-                      'Mods': game.mods,
-                      'TTL': str(ttl),
-                      })
-        return games
+        try:
+            now = datetime.datetime.now()
+            games = []
+            currentGames = db.GqlQuery("SELECT * FROM Game where lastPingTime >= :1 ORDER BY lastPingTime DESC", now - Server.STALE_DURATION)
+            for game in currentGames:
+                ttl = Server.STALE_DURATION.seconds - (now - game.lastPingTime).seconds
+                games.append({
+                          'Name': game.name,
+                          'Address': "%s:%d" % (game.ip, game.port),
+                          'State': str(game.state),
+                          'Players': str(game.players),
+                          'Map': game.map,
+                          'Mods': game.mods,
+                          'TTL': str(ttl),
+                          })
+            return games
+        except Exception, exception:
+            logging.exception(exception)
     GetGames = classmethod(GetGames)
     
 class Game(db.Model):
